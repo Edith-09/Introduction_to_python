0bb15c2b7934eeb3c46913011dcf99a8ba369a60
Edith-09 <kosgeyedith020@gmail.com>
2025-11-12 10:56:30 +0300

    Update of new files

---
+++ functions.py
@@ -0,0 +1,26 @@
+def my_function():
+    print("Hello world!")
+my_function()
+def salutation():
+    hello="Good morning Edith!"
+    print(hello)
+salutation()
+
+def salute(name):
+    print(f"Good afternoon {name}!")
+salute("Edith")
+salute("John")
+salute("Sara")
+def person(name,age,gender):
+    print(f"My name is {name}, I am {age} years old and  I am a {gender}.")
+person("Edith",20,"Female")
+person("John",22,"Male")
+person("Sara",19,"Female")
+
+def addition(a,b):
+    result=a+b
+    return result
+print("The sum is:", addition(5,3))
+#write a function tha will calculate an individual's BMI
+
+  
